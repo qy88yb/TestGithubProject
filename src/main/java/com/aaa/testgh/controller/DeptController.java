13391a3faf9a139ef2d1c76c84356d9aab753319
github qy88yb <github 771738663@qq.com>
2018-11-29 16:38:16 +0800

    王者荣耀

--- src/main/java/com/aaa/testgh/controller/DeptController.java
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -10,6 +10,7 @@ public class DeptController {
     public static void main(String[] args) {
         System.out.println("测试github提交。。。。。。。。。。。。。。");
         System.out.println("啦啦啦啦啦啊");
+        System.out.println("王者荣耀，我是菜鸟");
     }
 
 
