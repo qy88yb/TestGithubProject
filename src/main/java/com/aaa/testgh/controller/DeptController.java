7f6b72c3b77b2da058ec1c5219209aee210e9511
github qy88yb <github 771738663@qq.com>
2018-11-29 16:34:50 +0800

    111

--- src/main/java/com/aaa/testgh/controller/DeptController.java
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -8,7 +8,8 @@ package com.aaa.testgh.controller;
  */
 public class DeptController {
     public static void main(String[] args) {
-        System.out.println("测试github提交。。。。。。。。。。。。。。。");
+        System.out.println("测试github提交。。。。。。。。。。。。。。");
+        System.out.println("啦啦啦啦啦啊");
     }
 
 
